13a9ede5f517110ed268d7ae743d65d20f317af2
luckshya <luckshyaverma@gmail.com>
2024-09-04 15:33:41 +0530

    add center slot bean

---
+++ src/com/flipkart/bean/FlipfitCenterSlot.java
@@ -0,0 +1,51 @@
+package com.flipkart.bean;
+
+import java.time.LocalTime;
+
+public class FlipfitCenterSlot {
+    private String id;
+    private FlipfitCentre centre;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int seatLimit;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public FlipfitCentre getCentre() {
+        return centre;
+    }
+
+    public void setCentre(FlipfitCentre centre) {
+        this.centre = centre;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getSeatLimit() {
+        return seatLimit;
+    }
+
+    public void setSeatLimit(int seatLimit) {
+        this.seatLimit = seatLimit;
+    }
+}
